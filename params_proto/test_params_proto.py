f0215ad15a267a01bb3c2ec492673c9b6c45d0c4
Ge Yang <yangge1987@gmail.com>
2018-01-13 03:01:14 -0600

    use Proto for the arguments

--- params_proto/test_params_proto.py
+++ params_proto/test_params_proto.py
@@ -1,6 +1,6 @@
 import sys
 
-from .params_proto import is_hidden, cli_parse, ParamsProto, proto_signature
+from .params_proto import is_hidden, cli_parse, Proto, ParamsProto, proto_signature
 
 
 def test_decorator():
@@ -36,42 +36,22 @@ def test_cli_proto_simple():
     @cli_parse
     class G(ParamsProto):
         """Supervised MAML in tensorflow"""
-        npts = 100,
+        npts = 100
 
-    if sys.version_info >= (3, 6):
-        assert G.npts == (100,)
-    else:
-        assert G.npts == 100
+    assert G.npts == 100
 
 
 def test_cli_proto():
-    if sys.version_info >= (3, 6):
-        script = """
-@cli_parse
-class G(ParamsProto):
-    \"\"\"Supervised MAML in tensorflow\"\"\"
-    npts: "number of points to sample from distribution" = 100
-    num_epochs: "number of epochs to train" = 70000
-    num_tasks: "number of tasks in the inner loop" = 10
-    num_grad_steps: "number of gradient descent steps in the inner loop" = 1
-    num_points_sampled: "effectively the k-shot" = 10
-    eval_grad_steps: "the grad steps evaluated with full sample" = [0, 1, 10]
-    fix_amp: "controls the sampling, fix the amplitude of the sample distribution if True" = False
-        """
-    else:
-        script = """
-@cli_parse
-class G(ParamsProto):
-    \"\"\"Supervised MAML in tensorflow\"\"\"
-    npts = 100, "number of points to sample from distribution"
-    num_epochs = 70000, "number of epochs to train"
-    num_tasks = 10, "number of tasks in the inner loop"
-    num_grad_steps = 1, "number of gradient descent steps in the inner loop"
-    num_points_sampled = 10, "effectively the k-shot"
-    eval_grad_steps = [0, 1, 10], "the grad steps evaluated with full sample"
-    fix_amp = False, "controls the sampling, fix the amplitude of the sample distribution if True"
-        """
-    exec(script, globals())
+    @cli_parse
+    class G(ParamsProto):
+        """Supervised MAML in tensorflow"""
+        npts = Proto(100, help="number of points to sample from distribution")
+        num_epochs = Proto(70000, help="number of epochs to train")
+        num_tasks = Proto(10, help="number of tasks in the inner loop")
+        num_grad_steps = Proto(1, help="number of gradient descent steps in the inner loop")
+        num_points_sampled = Proto(10, help="effectively the k-shot")
+        eval_grad_steps = Proto([0, 1, 10], help="the grad steps evaluated with full sample")
+        fix_amp = Proto(False, help="controls the sampling, fix the amplitude of the sample distribution if True")
 
     assert G.npts == 100
     G.npts = 10
@@ -83,17 +63,12 @@ class G(ParamsProto):
 
 
 def test_proto_signature():
-    if sys.version_info >= (3, 6):
-        script = "@cli_parse\n" \
-                 "class G(ParamsProto):\n" \
-                 "    \"\"\"some parameter proto\"\"\"\n" \
-                 "    npts: \"number of points to sample from distribution\" = 100\n"
-    else:
-        script = "@cli_parse\n" \
-                 "class G(ParamsProto):\n" \
-                 "    \"\"\"some parameter proto\"\"\"\n" \
-                 "    npts = 100, \"number of points to sample from distribution\"\n"
-    exec(script, globals())
+    @cli_parse
+    class G(ParamsProto):
+        """some parameter proto"""
+        n = 1
+        npts = Proto(100, help="number of points to sample from distribution")
+        ok = True
 
     @proto_signature(G._proto)
     def main_demo(**kwargs):
@@ -106,20 +81,13 @@ def test_proto_signature():
 
     assert main_demo(npts=10) == 10
     print("main_demo<Function> signature:", inspect.signature(main_demo))
-    assert str(inspect.signature(main_demo)) == "(npts=100)"
+    assert str(inspect.signature(main_demo)) == "(n=1, npts=100, ok=True)"
 
 
 def test_proto_to_dict():
-    if sys.version_info >= (3, 6):
-        script = "@cli_parse\n" \
-                 "class G(ParamsProto):\n" \
-                 "    \"\"\"some parameter proto\"\"\"\n" \
-                 "    npts: \"number of points to sample from distribution\" = 100\n"
-    else:
-        script = "@cli_parse\n" \
-                 "class G(ParamsProto):\n" \
-                 "    \"\"\"some parameter proto\"\"\"\n" \
-                 "    npts = 100, \"number of points to sample from distribution\"\n"
-    exec(script, globals())
+    @cli_parse
+    class G(ParamsProto):
+        """some parameter proto"""
+        npts: "number of points to sample from distribution" = 100
 
     assert vars(G) == {'npts': 100}
