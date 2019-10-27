e86ffa0cfcd508ed7eab388f2c95c163d334c7e1
Ge Yang <yangge1987@gmail.com>
2019-10-27 21:03:28 +0000

    Add working test, Use Meta class for dictionary retrieval.

---
+++ params_proto/test_neo_proto.py
@@ -0,0 +1,82 @@
+from params_proto.neo_proto import ParamsProto, get_children
+
+
+def test_class():
+    """The class should be usable as a namespace directly."""
+
+    print("*************************************")
+    class Root(ParamsProto, prefix='root'):
+        launch_type = 'borg'
+
+    assert vars(Root) == {'launch_type': 'borg'}
+    assert Root.launch_type == "borg"
+    r = Root()
+    assert r.launch_type == "borg"
+
+    # Note: If you override the value on the master class
+    #  it gets updated and propagates to the instance.
+    Root.launch_type = "others"
+    assert Root.launch_type == "others", "same usage as before"
+    r = Root()
+    assert r.launch_type == "others"
+
+    r = Root({"root.launch_type": 'local'})
+    assert r.launch_type == "local"
+
+
+def test_dependency():
+    class Root(ParamsProto, prefix='root'):
+        launch_type = 'borg'
+
+    class SomeConfig(ParamsProto, prefix='resource'):
+        fixed = "default"
+        some_item = "default_value"
+
+        @get_children
+        def __init__(self, _deps=None, **children):
+            root = Root(_deps)  # this updates the root object.
+            if root.launch_type == "borg":
+                self.some_item = "new_value"
+            else:
+                self.some_item = self.__class__.some_item
+
+    s = SomeConfig({"root.launch_type": 'local'})
+    assert s.some_item == "default_value"
+
+    s = SomeConfig({"root.launch_type": 'borg'})
+    assert s.some_item == "new_value"
+
+
+def test_prefix():
+    class Root(ParamsProto, prefix='root'):
+        launch_type = 'borg'
+
+    class Teacher(ParamsProto, prefix="resources.teacher"):
+        cell = None
+        autopilot = False
+
+        @get_children
+        def __init__(self, _deps, **children):
+            # if Root(_deps).launch_type != 'local':
+            #     self.replicas_hint = children.get('replicas_hint', 26)
+            super().__init__(**children)
+
+    class Resources(ParamsProto):
+
+        @get_children
+        def __init__(self, _deps=None, **children):
+            r = Root(_deps)
+            print(children)
+            self.item = children.get('teacher', None)
+            self.teacher = Teacher(_deps, replicas_hint=26 if r.launch_type == 'borg' else 1)
+            self.bad_teacher = Teacher(_deps, _prefix="bad_teacher", replicas_hint=26 if r.launch_type == 'borg' else 1)
+
+    sweep_param = {
+        "root.launch_type": "local",
+        "resources.teacher.replica_hint": 10,
+    }
+
+    gd = Resources(sweep_param)
+    print(vars(gd))
+    print(gd.teacher)
+    print(gd.bad_teacher)
