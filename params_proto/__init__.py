84987bccdeb5b7519f494e5391b6a138ac600445
Ge Yang <yangge1987@gmail.com>
2019-11-17 21:49:52 +0000

    fix init file

--- params_proto/__init__.py
+++ params_proto/__init__.py
@@ -1,5 +1,7 @@
 from . import utils
 from . import neo_proto
+from . import neo_hyper
 from . import hyper
-from .params_proto import proto, prefix_proto, prefix_parse, proto_partial, PREFIXES, cli_parse, Proto, BoolFlag, ParamsProto, proto_signature, is_hidden
+from .params_proto import proto, prefix_proto, prefix_parse, proto_partial, PREFIXES, cli_parse, Proto, BoolFlag, \
+    ParamsProto, proto_signature, is_hidden
 
