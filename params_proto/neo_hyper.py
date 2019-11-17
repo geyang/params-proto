1c695329d108bb11f54e245493e15b46b6d42cd5
Ge Yang <yangge1987@gmail.com>
2019-11-17 21:47:22 +0000

    Add New Hyper Parameter Sweep Design:
    ```
    def test_set():
        class G(ParamsProto):
            start_seed = 10
            discern_flag = True
            env_name = "dm_lab"
            batch_size = 5
            replicas_hint = ['yo', 'hey']
    
        with Sweep(G) as sweep:
            G.start_seed = 20
            G.discern_flag = False
    
        for override in sweep:
            assert G.discern_flag is False
            assert override == {'G.discern_flag': False, 'G.start_seed': 20}
    ```

--- params_proto/neo_hyper.py
+++ params_proto/neo_hyper.py
@@ -1,8 +1,8 @@
 import itertools
 from collections import namedtuple
 from contextlib import contextmanager
+from functools import partial
 from typing import TypeVar, ContextManager, Iterable
-from params_proto.neo_proto import is_private
 
 
 def dot_join(*keys):
@@ -26,29 +26,6 @@ def key_items(d, prefix=None):
         yield [Item(_, v) if _ else v for v in vs]
 
 
-# support nested query.
-class ProxyObject(object):
-    def __init__(self, hook, prefix=None):
-        self.__prefix = prefix
-        self.__hook = hook
-
-    @property
-    def __dict__(self):
-        return {k: v for k, v in super().__dict__.items() if not is_private(k)}
-
-    def __setattr__(self, key, value):
-        if key.startswith("_ProxyObject_"):
-            return object.__setattr__(self, key, value)
-
-        self.__hook(dot_join(self.__prefix, key), value)
-
-    def __getattr__(self, item):
-        if item.startswith("_ProxyObject_"):
-            return object.__getattribute__(self, item)
-
-        return ProxyObject(self.__hook, prefix=dot_join(self.__prefix, item))
-
-
 def flatten_items(row) -> Iterable[Item]:
     if isinstance(row, Item):
         yield row
@@ -63,54 +40,82 @@ T = TypeVar('ParamsProto')
 
 
 class Sweep:
-    def __init__(self):
+    root = {}
+
+    def __init__(self, *args: type):
+        self.root = {p._prefix: p for p in args}
         self.stack = [[]]  # root stack frame
+
+    def __enter__(self):
+        self.stack.append([])
+
+        set_hook = lambda _, k, v: self.set_param(k, [v], prefix=proto._prefix)
+        for proto in self.root.values():
+            proto._add_hook(set_hook)
+
+        return self
+
+    def __exit__(self, *args):
+        for proto in self.root.values():
+            proto._pop_hooks()
+
+        frame = self.stack.pop(-1)
+        result = itertools.product(*key_items(frame))
+        self.set_param(None, result)
 
     def __iter__(self):
         root_stack = self.stack[-1]
         key, rows = root_stack[0]
         assert key is None, "this is the root stack"
         for row in rows:
-            a = flatten_items(row)
-            yield dict(a)
+            override = dict(flatten_items(row))
+
+            # Override the original object
+            for proto in self.root.values():
+                proto._update(override)
+
+            yield override
 
     def set_param(self, name, params, prefix=None):
         item = Item(dot_join(prefix, name), params)
         self.stack[-1].append(item)
 
+    @property
     @contextmanager
-    def product(self, Strut: T) -> ContextManager[T]:
+    def product(self) -> ContextManager[None]:
         self.stack.append([])
-        proxy = ProxyObject(self.set_param, prefix=Strut._prefix)
         try:
-            yield proxy
+            for proto in self.root.values():
+                proto._add_hook(lambda _, *args: self.set_param(*args, prefix=proto._prefix))
+            yield None
         finally:
+            for proto in self.root.values():
+                proto._pop_hooks()
+
             frame = self.stack.pop(-1)
             result = itertools.product(*key_items(frame))
             self.set_param(None, result)
 
+    @property
     @contextmanager
-    def zip(self, Strut: T) -> ContextManager[T]:
+    def zip(self) -> ContextManager[T]:
         self.stack.append([])
-        proxy = ProxyObject(self.set_param, prefix=Strut._prefix)
         try:
-            yield proxy
+            for proto in self.root.values():
+                proto._add_hook(lambda _, *args: self.set_param(*args, prefix=proto._prefix))
+            yield None
         finally:
+            for proto in self.root.values():
+                proto._pop_hooks()
+
             frame = self.stack.pop(-1)
             result = zip(*key_items(frame))
             self.set_param(None, result)
 
+    @property
     @contextmanager
-    def set(self, Strut: T) -> ContextManager[T]:
-        self.stack.append([])
-        _ = lambda k, v: self.set_param(k, [v])
-        proxy = ProxyObject(_, prefix=Strut._prefix)
+    def set(self) -> ContextManager[T]:
         try:
-            yield proxy
+            yield self.__enter__()
         finally:
-            frame = self.stack.pop(-1)
-            result = itertools.product(*key_items(frame))
-            self.set_param(None, result)
-
-
-sweep = Sweep()
+            self.__exit__()
---
+++ test_params_proto/test_neo_hyper.py
@@ -0,0 +1,159 @@
+from params_proto.neo_proto import ParamsProto
+from params_proto.neo_hyper import Sweep, dot_join
+
+
+def test_dot_join():
+    assert dot_join(None, None) is None
+
+
+def test_setter_and_getter_hook():
+    # usage
+    class G(ParamsProto):
+        start_seed = 10
+        discern_flag = True
+        env_name = "dm_lab"
+        batch_size = 5
+        replicas_hint = ['yo', 'hey']
+
+    def setter(Proto, item, value):
+        print(Proto, item, value)
+
+    print(vars(G))
+    G._add_hook(setter)
+
+    assert G.start_seed == 10
+    G.start_seed = 20
+    G._pop_hooks()
+
+
+def test_product():
+    # usage
+    class G(ParamsProto):
+        start_seed = 10
+        discern_flag = True
+        env_name = "dm_lab"
+        batch_size = 5
+        replicas_hint = ['yo', 'hey']
+
+    with Sweep(G) as sweep:
+        with sweep.product:
+            G.start_seed = range(2)
+            G.discern_flag = [True, False]
+
+    all = [*sweep]
+    assert all == [{'G.discern_flag': True, 'G.start_seed': 0},
+                   {'G.discern_flag': False, 'G.start_seed': 0},
+                   {'G.discern_flag': True, 'G.start_seed': 1},
+                   {'G.discern_flag': False, 'G.start_seed': 1}]
+
+
+def test_zip():
+    # usage
+    class G(ParamsProto):
+        start_seed = 10
+        discern_flag = True
+        env_name = "dm_lab"
+        batch_size = 5
+        replcas_hint = ['yo', 'hey']
+
+    with Sweep(G) as sweep:
+        with sweep.zip:
+            G.env_name = ['small_env', 'large_env']
+            G.batch_size = [10, 50]
+
+    all = [*sweep]
+    assert all == [{'G.batch_size': 10, 'G.env_name': 'small_env'},
+                   {'G.batch_size': 50, 'G.env_name': 'large_env'}]
+
+
+def test_product_zip():
+    class G(ParamsProto):
+        start_seed = 10
+        discern_flag = True
+        env_name = "dm_lab"
+        batch_size = 5
+        replcas_hint = ['yo', 'hey']
+
+    with Sweep(G) as sweep:
+        with sweep.product:
+            G.start_seed = range(2)
+            G.discern_flag = [True, False]
+
+            with sweep.zip:
+                G.env_name = ['small_env', 'large_env']
+                G.batch_size = [10, 50]
+
+    all = [*sweep]
+    assert len(all) == 2 * 2 * 2
+    assert all == [{'G.batch_size': 10, 'G.discern_flag': True, 'G.env_name': 'small_env', 'G.start_seed': 0},
+                   {'G.batch_size': 50, 'G.discern_flag': True, 'G.env_name': 'large_env', 'G.start_seed': 0},
+                   {'G.batch_size': 10, 'G.discern_flag': False, 'G.env_name': 'small_env', 'G.start_seed': 0},
+                   {'G.batch_size': 50, 'G.discern_flag': False, 'G.env_name': 'large_env', 'G.start_seed': 0},
+                   {'G.batch_size': 10, 'G.discern_flag': True, 'G.env_name': 'small_env', 'G.start_seed': 1},
+                   {'G.batch_size': 50, 'G.discern_flag': True, 'G.env_name': 'large_env', 'G.start_seed': 1},
+                   {'G.batch_size': 10, 'G.discern_flag': False, 'G.env_name': 'small_env', 'G.start_seed': 1},
+                   {'G.batch_size': 50, 'G.discern_flag': False, 'G.env_name': 'large_env', 'G.start_seed': 1}]
+
+
+def test_set():
+    class G(ParamsProto):
+        start_seed = 10
+        discern_flag = True
+        env_name = "dm_lab"
+        batch_size = 5
+        replicas_hint = ['yo', 'hey']
+
+    with Sweep(G) as sweep:
+        G.start_seed = 20
+        G.discern_flag = False
+
+    for override in sweep:
+        assert G.discern_flag is False
+        assert override == {'G.discern_flag': False, 'G.start_seed': 20}
+
+
+def test_set_advanced():
+    # usage
+    class G(ParamsProto):
+        start_seed = 10
+        discern_flag = True
+        env_name = "dm_lab"
+        batch_size = 5
+        replicas_hint = ['yo', 'hey']
+
+    with Sweep(G) as sweep:
+        G.replicas_hint = 12
+
+        with sweep.zip:
+            G.env_name = ['small', 'large']
+            G.batch_size = [10, 50]
+
+    all = [*sweep]
+    assert len(all) == 2
+
+
+def test_set_advanced_2():
+    # usage
+    class G(ParamsProto):
+        null_attribute = True
+        start_seed = 10
+        discern_flag = True
+        env_name = "dm_lab"
+        batch_size = 5
+        replicas_hint = ['yo', 'hey']
+
+    with Sweep(G) as sweep:
+        G.null_attribute = None
+        G.replicas_hint = 12
+
+        with sweep.product:
+            G.start_seed = range(15)
+
+            with sweep.zip:
+                G.env_name = ['small', 'large', 'xl']
+                G.batch_size = [10, 50, 100]
+
+    all = [*sweep]
+    assert len(all) == 45
+
+
