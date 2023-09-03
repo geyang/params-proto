166fc996a646de6692f155c171d3c1b93c69b130
Ge Yang <yangge1987@gmail.com>
2023-09-03 14:52:53 -0400

    add dependencies

--- setup.py
+++ setup.py
@@ -7,22 +7,29 @@ with open(path.join(path.abspath(path.dirname(__file__)), 'README'), encoding='u
 with open(path.join(path.abspath(path.dirname(__file__)), 'VERSION'), encoding='utf-8') as f:
     VERSION = f.read()
 
-setup(name="params_proto",
-      description= "Modern Hyper Parameter Management for Machine Learning",
-      long_description=LONG_DESCRIPTION,
-      version=VERSION,
-      url="https://github.com/episodeyang/params_proto",
-      author="Ge Yang",
-      author_email="yangge1987@gmail.com",
-      license=None,
-      keywords=["params_proto", "decorator", "argparse",
-                "argcomplete", "auto-completion", "autocomplete",
-                "shell arguments", "argument parser"],
-      classifiers=[
-          "Development Status :: 4 - Beta",
-          "Intended Audience :: Science/Research",
-          "Programming Language :: Python :: 3"
-      ],
-      packages=[p for p in find_packages() if "test" not in p],
-      install_requires=["waterbear>=2.6.5", "argparse", "argcomplete", "typing", "expandvars"]
-      )
+setup(
+    name="params_proto",
+    description="Modern Hyper Parameter Management for Machine Learning",
+    long_description=LONG_DESCRIPTION,
+    version=VERSION,
+    url="https://github.com/episodeyang/params_proto",
+    author="Ge Yang",
+    author_email="yangge1987@gmail.com",
+    license=None,
+    keywords=["params_proto", "decorator", "argparse",
+              "argcomplete", "auto-completion", "autocomplete",
+              "shell arguments", "argument parser"],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Science/Research",
+        "Programming Language :: Python :: 3"
+    ],
+    packages=[p for p in find_packages() if "test" not in p],
+    install_requires=[
+        "waterbear>=2.6.8",
+        "argparse",
+        "argcomplete",
+        "typing",
+        "expandvars",
+    ]
+)
